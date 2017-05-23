d8224919f4e93ea25d5248252bbb5814da6c7276
Gabriel Vacaliuc <gabe.vacaliuc@gmail.com>
2017-05-23 02:59:38 -0700

    environment setup batch file

---
+++ setup_environment.bat
@@ -0,0 +1,13 @@
+set OPENCV_DIR=C:\local\opencv\build\x64\vc14\lib
+set BOOST_ROOT=C:\local\boost_1_59_0
+set BOOST_LIBRARYDIR=%BOOST_ROOT%\lib64-msvc-14.0
+set TBB_ROOT_DIR=C:\local\tbb2017_20170412oss
+set TBB_LIBRARY=%TBB_ROOT_DIR%\lib\intel64\vc14\tbb.lib
+set TBB_INCLUDE=%TBB_ROOT_DIR%\include
+set ZLIB_INCLUDE_DIR=C:\local\zlib-1.2.11
+set ZLIB_LIBRARY=%ZLIB_INCLUDE_DIR%\build\zlibd.lib
+set PNG_INCLUDE_DIR=C:\local\lpng1629
+set PNG_LIBRARY=%PNG_INCLUDE_DIR%\build\libpng16d.lib
+set JPEG_INCLUDE_DIR=C:\local\jpeg-9a-cmake
+set JPEG_LIBRARY=%JPEG_INCLUDE_DIR%\build\libjpeg.lib
+
